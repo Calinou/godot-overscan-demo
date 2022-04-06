8f4dd195e32e15bd961bd150d6da43fb876198b2
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-04-06 20:45:53 +0200

    Fix script for 4.0alpha6

--- overscan.gd
+++ overscan.gd
@@ -11,7 +11,7 @@ func _on_overscan_slider_value_changed(value: float) -> void:
 	anchor_left = -value + 1
 	anchor_bottom = value
 	anchor_right = value
-	
+
 	# Calculate new FOV to look similar to the base FOV in the actual window.
-	var aspect := (rect_size.x / DisplayServer.window_get_size().x)
+	var aspect := (size.x / DisplayServer.window_get_size().x)
 	camera.fov = atan(tan(camera_base_fov * PI / 360.0) * aspect) * 360.0 / PI
